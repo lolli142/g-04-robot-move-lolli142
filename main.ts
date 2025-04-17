4862339e804c97016d99188dc2556be57f5dae47
lolli142 <alaa.y.hamdan@gmail.com>
2025-04-17 07:43:07 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,18 @@
+basic.forever(function () {
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 0)
+    basic.showLeds(`
+        . # . # .
+        # # # # #
+        # # # # #
+        . # # # .
+        . . # . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
